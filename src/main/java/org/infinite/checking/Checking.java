aeeeb164dad5023645f1374986c2c946a108ccb5
sasikirank <sasikirank@ICS-LT-98L96V3>
2023-10-16 00:34:13 +0530

    Checking

---
+++ src/main/java/org/infinite/checking/Checking.java
@@ -0,0 +1,20 @@
+package org.infinite.checking;
+
+public class Checking {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			public void fruits(){
+				int a=4;
+				int b=5;
+				int c=a+b;
+			  System.out.println("answer:" );
+			}
+			
+		}
+
+
+	}
+
+}
